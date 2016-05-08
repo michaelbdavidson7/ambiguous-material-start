2c335fcaa5035744ffcd1f586b9aa285f8690a2b
Michael <MichaelBDavidson7@gmail.com>
2016-05-08 15:44:20 -0400

    local

---
+++ app/src/users/Users.js
@@ -0,0 +1,15 @@
+(function(){
+  'use strict';
+
+  // Prepare the 'users' module for subsequent registration of controllers and delegates
+  angular.module('users', [ 'ngMaterial' ]);
+
+
+  angular.module('users')
+         .controller('index', [index]);
+
+  function index(userService, $mdSidenav, $mdBottomSheet, $timeout, $log) { 
+      console.log('index1');
+  };
+
+})();
